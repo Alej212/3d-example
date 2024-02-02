c94533822700b60c8637c78c9d72243667842ce8
Alej <alejandrocevedo09092002@gmail.com>
2024-02-02 17:17:28 -0400

    init project

---
+++ app.vue
@@ -0,0 +1,28 @@
+<template>
+  <TresCanvas window-size clear-color="black">
+    <TresPerspectiveCamera/>
+    <OrbitControls/>
+    <TresMesh ref="sphereRef">
+      <TresSphereGeometry></TresSphereGeometry>
+      <TresMeshNormalMaterial></TresMeshNormalMaterial>
+    </TresMesh>
+    <TresGridHelper/>
+  </TresCanvas>
+</template>
+
+<script setup>
+import { TresCanvas, useRenderLoop } from "@tresjs/core";
+import { OrbitControls } from "@tresjs/cientos";
+//import {  } from "@tresjs/post-processing";
+
+const sphereRef = ref()
+
+const { onLoop } = useRenderLoop()
+
+onLoop(({elapsed}) => {
+  if (sphereRef.value) {
+    sphereRef.value.position.y = Math.sin(elapsed)
+  }
+})
+
+</script>
